05e29003a55d2d8954847a8f33e5a039e5573f85
sincoo <37761656+sincoo@users.noreply.github.com>
2020-03-31 21:38:09 +0800

    Add files via upload

--- assembler_vs/Assembler.cpp
+++ assembler_vs/Assembler.cpp
@@ -1,6 +1,5 @@
-﻿#include "pch.h"
+#include "pch.h"
 #include "Assembler.h"
-//#define HEX_OUTPUT
 Assembler::Assembler(int id, int ii, int ll, int II)
 {
 	in1 = "XXXXXXXX";
@@ -18,12 +17,17 @@ Assembler::Assembler(int id, int ii, int ll, int II)
 	DirectAddrMem = "XXXXXXXX";
 	InMem = "XXXXXXXX";
 	Offset = "XXXX";
+	Offsetex = "XXX";
+	IItype = "X";
+	increaseFlag = "XX";
 	Task_PackageNum = "XXXXX";
 	Package_Index = "XXXXX";
 	Index_PE = "XXXXXXXX";
 	Iteration_PEA = "XXXXXXX";
 	Iteration_PE = "XXXXXXX";
+	Iteration_PE_ex = "X";
 	Initial_Idle = "XXXXXX";
+	Initial_Idle_ex = "XX";
 	Iteration_Line = "XX";
 	Count = "XXXXXX";
 	type = 0;
@@ -38,19 +42,31 @@ Assembler::~Assembler()
 {
 }
 
-string D2B(int n) {
+string D2B(int &n) {    //modified for GR 8-15
 	string result;
 	switch (n)
 	{
-	case 0:result = "000"; break;
-	case 1:result = "001"; break;
-	case 2:result = "010"; break;
-	case 3:result = "011"; break;
-	case 4:result = "100"; break;
-	case 5:result = "101"; break;
-	case 6:result = "110"; break;
-	case 7:result = "111"; break;
-	default:result = "000"; break;
+	case 0:result = "00000"; break;
+	case 1:result = "00001"; break;
+	case 2:result = "00010"; break;
+	case 3:result = "00011"; break;
+	case 4:result = "00100"; break;
+	case 5:result = "00101"; break;
+	case 6:result = "00110"; break;
+	case 7:result = "00111"; break;
+	case 8:result = "01000"; break;
+	case 9:result = "01001"; break;
+	case 10:result = "01010"; break;
+	case 11:result = "01011"; break;
+	case 12:result = "01100"; break;
+	case 13:result = "01101"; break;
+	case 14:result = "01110"; break;
+	case 15:result = "01111"; break;
+	case 16:result = "10000"; break;
+	case 17:result = "10001"; break;
+	case 18:result = "10010"; break;
+	case 19:result = "10011"; break;
+	default:result = "00000"; break;
 	}
 	return result;
 }
@@ -79,7 +95,7 @@ long long int B2D(string pre)
 	int result = 0;
 	for (int i = 0; i < length; i++)
 	{
-		result += (int)((charToDigit(pre[i]))*pow(2, length - 1 - i));
+		result += ((charToDigit(pre[i]))*pow(2, length - 1 - i));
 	}
 	return result;
 }
@@ -108,7 +124,7 @@ string D2H(long long int pre) {
 	}
 	string res = oss.str();
 	oss.str("");//清空原数据流
-	for (int i = 0; i != res.length(); i++) {
+	for (int i = 0; i < res.length(); i++) {
 		oss << res[res.length() - i - 1];
 	}
 	return oss.str();
@@ -208,7 +224,7 @@ void Assembler::groupInstructions(InstructionList & instructionList, StringList 
 
 void Assembler::clearComments(StringList &sL)
 {
-	for (int i = 0; i != sL.size(); i++) {
+	for (int i = 0; i < sL.size(); i++) {
 		if (sL[i] == "") {
 			sL.erase(i + sL.begin());
 			i--;
@@ -219,7 +235,7 @@ void Assembler::clearComments(StringList &sL)
 		}
 		else {
 			string *temp = &sL[i];
-			for (int j = 0; j != temp->size(); j++) {
+			for (int j = 0; j < temp->size(); j++) {
 				if ((*temp)[j] == ' '&& j==0) {
 					temp->erase(j,1);
 					j--;
@@ -229,16 +245,22 @@ void Assembler::clearComments(StringList &sL)
 	}
 }
 
-void Assembler::transformOpcode(string &opcode)
+void Assembler::transformOpcode(string &opcode,int PE_ID)
 {
 	if (opcode == "%router") {
 		opcode = "00001";
 	}
-	else if (opcode == "%load") {
+	else if (opcode == "%load"&& PE_ID <= 7) {
 		opcode = "00";
 	}
-	else if (opcode == "%store") {
+	else if (opcode == "%load"&& PE_ID > 7) {
+		opcode = "10";
+	}
+	else if (opcode == "%store" && PE_ID<=7) {
 		opcode = "01";
+	}
+	else if (opcode == "%store" && PE_ID > 7) {
+		opcode = "11";
 	}
 	else if (opcode == "%add") {
 		opcode = "00010";
@@ -273,7 +295,7 @@ void Assembler::transformOpcode(string &opcode)
 	else if (opcode == "%xor") {
 		opcode = "01000";
 	}
-	else if (opcode == "%not") {
+	else if (opcode == "%abs") {
 		opcode = "01001";
 	}
 	else if (opcode == "%sel") {
@@ -309,7 +331,7 @@ void Assembler::transformIndex(string &index) {
 	//	return 0;//左上顶角
 	//	return 1;//右上顶角
 	//	return 2;//左下顶角
-	//	return 3;//右下顶角
+	//	return 3;//右上顶角
 	//	return 4;//上边沿
 	//	return 5;//下边沿
 	//	return 6;//左边沿
@@ -526,6 +548,7 @@ void Assembler::transformIndex(string &index) {
 
 string IntToBinaryString(int n,int length) {
 	string binary="";
+	if (n < 0) n = -n;
 	while (n / 2 != 1 && n!=0) {		
 		binary = n % 2 == 0 ? "0" + binary : "1" + binary;
 		n = n / 2;
@@ -546,19 +569,16 @@ string IntToBinaryString(int n,int length) {
 void Assembler::tranformOut(string &temp)
 {
 	string number = "";
-	for (int i = 2; i != temp.size(); i++) {
+	for (int i = 2; i < temp.size(); i++) {
 		number = number + temp[i];
 	}
-	int Number = (int)atof(number.c_str());
+	int Number = atof(number.c_str());
 	if (temp[0] == 'L' && temp[1] == 'R') {
-		temp = "0000" + D2B(Number);
+		temp = "00" + D2B(Number);
 	}
 	else if (temp[0] == 'G' && temp[1] == 'R')
 	{
-		if (Number > 15)
-			temp = "0101" + D2B(Number - 16);
-		else
-			temp = "0100" + D2B(Number);
+		temp = "01" + D2B(Number);
 	}
 }
 
@@ -574,7 +594,7 @@ void Assembler::transformIn(string &temp)
 			source = "100";
 		}
 		string index;
-		for (int i = 3; i != temp.size(); i++) {
+		for (int i = 4; i < temp.size(); i++) {
 			index = index + temp[i];
 		}
 		
@@ -591,40 +611,43 @@ void Assembler::transformIn(string &temp)
 		}
 		else {
 			transformIndex(index);
-			temp = source + "0" + index;
+			if (temp[3] == 'E')
+				temp = source + "1" + index; //取流水线寄存器输出modified
+			else
+				temp = source + "0" + index; //取out输出
 		}
 	}
 	else if (temp[0] == 'L' && temp[1] == 'R') {
 		string source = "000";
 		string index = "";
-		for (int i = 2; i != temp.size(); i++) {
+		for (int i = 2; i < temp.size(); i++) {
 			index = index + temp[i];
 		}
-		int Number = (int)atof(index.c_str());
-		temp = source + "00" + D2B(Number);
+		int Number = atof(index.c_str());
+		temp = source + D2B(Number);
 	}
 	else if (temp[0] == 'G' && temp[1] == 'R') {
 		string source = "001";
 		string index = "";
-		for (int i = 2; i !=temp.size(); i++) {
+		for (int i = 2; i < temp.size(); i++) {
 			index = index + temp[i];
 		}
-		int Number = (int)atof(index.c_str());
-		temp = source + "00" + D2B(Number);
+		int Number = atof(index.c_str());
+		temp = source + D2B(Number);
 	}
 	else if (temp[0] == 'I' && temp[1] == 'M') {		
 		string index;
 		Imm = "1";		
-		for (int i = 2; i != temp.size(); i++) {
+		for (int i = 2; i < temp.size(); i++) {
 			index = index + temp[i];
 		}
-		int Number = (int)atof(index.c_str());
+		int Number = atof(index.c_str());
 		string binary = IntToBinaryString(Number, 8);
 		temp = binary;
 	}
 	else if (temp[0] == 'S' && temp[1] == 'M') {
 		string SM;
-		for (int i = 2; i !=temp.size(); i++) {
+		for (int i = 2; i < temp.size(); i++) {
 			SM = SM + temp[i];
 		}
 		int Number = atoi(SM.c_str());
@@ -638,7 +661,7 @@ void Assembler::transformIn4(string &temp)
 {
 	string out1_or_out2 = temp.substr(2, 1);
 	string index;
-	for (int i = 3; i != temp.size(); i++) {
+	for (int i = 3; i < temp.size(); i++) {
 		index = index + temp[i];
 	}
 	
@@ -652,27 +675,6 @@ void Assembler::transformIn4(string &temp)
 	}
 }
 
-void setIncreaseFlag(int& intOffset, string& Increase_Flag) {
-	if (intOffset < 0)
-	{
-		Increase_Flag = "10";
-	}
-	else if (intOffset == 0)
-	{
-		Increase_Flag = "00";
-	}
-	else
-	{
-		Increase_Flag = "01";
-	}
-}
-
-void setIItype(int& II, string& IItype) {
-		if (II < 0)
-			IItype = "1";
-		else
-			IItype = "0";
-}
 void Assembler::transformOperands(StringList &operands)
 {
 	if (type == 0) { //ALU
@@ -681,15 +683,11 @@ void Assembler::transformOperands(StringList &operands)
 		in1 = operands.at(0);
 		transformIn(in1);
 		if (opcode == "00001") {//route
-			in2 = in1;
+			transformIn(in1);
+			//in1 = "00000000";
+			in2 = operands.at(1);
 			transformIn(in2);
-			in1 = "00000000";
-			size = 1;
-		}
-		else if (opcode == "01001") {
-			transformIn(in1);		
-			in2 = "00000000";
-			size = 1;
+			size = 2;
 		}
 		else {
 			in2 = operands.at(1);
@@ -715,7 +713,7 @@ void Assembler::transformOperands(StringList &operands)
 		else {
 			in4 = "000000";
 		}
-		if ((int)operands.size() > size) {
+		if (operands.size() > size) {
 			out1 = operands.at(size);
 			tranformOut(out1);
 		}
@@ -727,8 +725,15 @@ void Assembler::transformOperands(StringList &operands)
 		if (intLength == 1) {
 			string iter_num = IntToBinaryString(intIteration, 5);
 			string iter_II = IntToBinaryString(intII, 3);
-			setIItype(intII, IItype);
-			iteration = "00" + iter_num + iter_II;
+			if (intII < 0) {
+				intII = -intII;
+				out3 = "1";
+			}
+			if (intII<=7)
+				iteration = "00" + iter_num + iter_II;
+			else
+				iteration = "01" + iter_num + "000";    //modified for ii>7 load iteration from GR
+
 		}
 		else {
 			iteration = "0000000000";
@@ -740,38 +745,63 @@ void Assembler::transformOperands(StringList &operands)
 		if (intLength == 1) {
 			string iter_num = IntToBinaryString(intIteration, 5);
 			string iter_II = IntToBinaryString(intII, 3);
-			setIItype(intII, IItype);
-			iteration = "00" + iter_num + iter_II;
+			if (intII < 0) {
+				intII = -intII;
+				IItype = "1";
+			}
+			else IItype = "0";
+			if (intII <= 7)
+				iteration = "00" + iter_num + iter_II;
+			else
+				iteration = "01" + iter_num + "000";    //modified for ii>7
 		}
 		else {
 			iteration = "0000000000";
 		}
 		DirectAddrMem = "00000000";
 		int size = 2;
-		if (opcode == "00") { //Load
+		if (opcode == "00" |opcode == "10") { //Load
 			AddrMem = operands.at(0);
 			transformIn(AddrMem);
 			int intOffset = atoi(operands.at(1).c_str());
-			Offset = IntToBinaryString(intOffset, 5);
-			Offset_extend = Offset[0];
-			Offset = Offset.substr(1, 4);
-			setIncreaseFlag(intOffset, Increase_Flag);			
+			if (intOffset < 0) {
+				intOffset = -intOffset;
+				increaseFlag = "10";
+			}
+			else if (intOffset == 0)
+				increaseFlag = "00";
+			else increaseFlag = "01";
+			if (intOffset >= 16) {
+				intOffset = intOffset - 16;
+				Offsetex = "100";
+			}
+			else Offsetex = "000";
+			Offset = IntToBinaryString(intOffset, 4);
 			InMem = "00000000";
 		}
-		else if (opcode == "01") { //Store		
+		else if (opcode == "01" | opcode =="11" ) { //Store		
 			AddrMem = operands.at(0);
 			transformIn(AddrMem);
 			InMem = operands.at(1);
 			int intOffset = atoi(operands.at(2).c_str());
-			Offset = IntToBinaryString(intOffset, 5);
-			Offset_extend = Offset[0];
-			Offset = Offset.substr(1, 4);
-			setIncreaseFlag(intOffset, Increase_Flag);
+			if (intOffset < 0) {
+				intOffset = -intOffset;
+				increaseFlag = "10";
+			}
+			else if (intOffset == 0)
+				increaseFlag = "00";
+			else increaseFlag = "01";
+			if (intOffset >= 16) {
+				intOffset = intOffset - 16;
+				Offsetex = "100";
+			}
+			else Offsetex = "000";
+			Offset = IntToBinaryString(intOffset, 4);
 			transformIn(AddrMem);
 			transformIn(InMem);
 			size = 3;
 		}
-		if ((int)operands.size() > size) {
+		if (operands.size() > size) {
 			out1 = operands.at(size);
 			tranformOut(out1);
 		}
@@ -811,20 +841,20 @@ int Assembler::groupPELocation(int PE_ID)
 	else if (1 <= PE_ID % 8 && PE_ID % 8 <= 3 && 1 <= PE_ID / 8 && PE_ID / 8 <= 3) {
 		return 8;//左上区
 	}
-	else if (1 <= PE_ID % 8 && PE_ID % 8 <= 3 && 4 <= PE_ID / 8 && PE_ID / 8<= 6) {
+	else if (1 <= PE_ID % 8 && PE_ID % 8 <= 3 && 4 <= PE_ID / 8 && PE_ID / 8 <= 6) {
 		return 9;//左下区
 	}
-	else if (4 <= PE_ID % 8 && PE_ID % 8 <= 6 && 1 <= PE_ID / 8 && PE_ID / 8<= 3) {
+	else if (4 <= PE_ID % 8 && PE_ID % 8 <= 6 && 1 <= PE_ID / 8 && PE_ID / 8 <= 3) {
 		return 10;//右上区
 	}
 	else if (4 <= PE_ID % 8 && PE_ID % 8 <= 6 && 4 <= PE_ID / 8 && PE_ID / 8 <= 6) {
 		return 11;//右下区
 	}
-	return 0;
 }
 
 string Assembler::transformAssembles(string &temp)
 {
+	int iteration_pe_temp;
 	if (temp[0] == 'P'&&temp[1] == 'E') {
 		return temp;
 	}
@@ -837,7 +867,7 @@ string Assembler::transformAssembles(string &temp)
 	string operand = opcode2Operand.at(1);
 	sperator = ",";
 	StringList operands = splitString(operand, sperator);
-	transformOpcode(opcode);
+	transformOpcode(opcode,PE_ID);
 	type = 0;
 	if (opcode.length() == 2) {
 		Func = "10";
@@ -853,80 +883,92 @@ string Assembler::transformAssembles(string &temp)
 	transformOperands(operands);
 
 	string transformed;
-	if (type == 0) { // ALU
-		if (Assembler::ifHEX) {
-			transformed = ConfigExtend + Func + in1 + in2 + in3 + in4 +
-				Imm + out1 + out2 + IItype + iteration + opcode;
-			string result("");
-			for (int j = 0; j != 16; j++)
-			{
-				int dec = (int)B2D(transformed.substr(4 * j, 4));
-				string hex = D2H(dec);
-				result = result + hex;
-			}
-			transformed = result;
+	if (type == 0) { // ALUF
+#ifdef  HEX_OUTPUT
+		//iteration = "1111111111";   //modified for load iteration from LR[8-11] or GR[16-19]
+		transformed = ConfigExtend +Func + in1 +  in2 +  in3 +  in4 + 
+			Imm +  out1 +  out2 +  out3 +  iteration +  opcode;
+		string result("");
+		for (int j = 0; j != 16; j++)
+		{
+			int dec = B2D(transformed.substr(4 * j, 4));
+			string hex = D2H(dec);
+			result = result + hex;
 		}
-		else {
-			transformed = ConfigExtend + "_" + Func + "_" + in1 + "_" + in2 + "_" + in3 + "_" + in4 + "_" +
-				Imm + "_" + out1 + "_" + out2 + "_" + IItype + "_" + iteration + "_" + opcode;
-		}	
-	}
+		transformed = result;
+#else
+		//iteration = "1111111111";   //modified for load iteration from LR[8-11] or GR[16-19]
+		transformed = ConfigExtend + "_" + Func + "_" + in1 + "_" + in2 + "_" + in3 + "_" + in4 + "_" +
+			Imm + "_" + out1 + "_" + out2 + "_" + out3 + "_" + iteration + "_" + opcode;
+#endif //  HEX_OUTPUT
 
+		
+	}
 	else if (type == 1) { //LS
-		if (Assembler::ifHEX) {
-			transformed = ConfigExtend + Func + AddrMem + DirectAddrMem
-				+ InMem + Offset + Offset_extend+"00" + out1 + "00000"+IItype + Increase_Flag
-				+ iteration + "000" + opcode;
-			string result("");
-			for (int j = 0; j != 16; j++)
-			{
-				int dec = (int)B2D(transformed.substr(4 * j, 4));
-				string hex = D2H(dec);
-				result = result + hex;
-			}
-			transformed = result;
-		}
-		else {
-			transformed = ConfigExtend + "_" + Func + "_" + AddrMem + "_" + DirectAddrMem
-				+ "_" + InMem + "_" + Offset + "_" + Offset_extend+"00" + "_" + out1 + "_" + "00000_"+IItype + Increase_Flag
-				+ "_" + iteration + "_" + "000" + "_" + opcode;
-		}
+#ifdef HEX_OUTPUT
+		transformed = ConfigExtend +  Func +  AddrMem + InMem
+			+  DirectAddrMem + Offset +  Offsetex  + out1 +  "00000" + IItype + increaseFlag
+			+  iteration + "000" +  opcode;
+		string result("");
+		for (int j = 0; j != 16; j++)
+		{
+			int dec = B2D(transformed.substr(4 * j, 4));
+			string hex = D2H(dec);
+			result = result + hex;
+	    }
+		transformed = result;
+#else
+		transformed = ConfigExtend + "_" + Func + "_" + AddrMem + "_" + InMem
+			+ "_" + DirectAddrMem + "_" + Offset + "_" + Offsetex + "_" + out1 + "_" + "00000" + "_" + IItype + "_" + increaseFlag
+			+ "_" + iteration + "_" + "000" + "_" + opcode;
+#endif // HEX_OUTPUT
 	}
 	else if (type == 2) { //top
 		Index_PE = IntToBinaryString(PE_ID, 8);	
 		Task_PackageNum = operands.at(0); //0开始计数
-		Task_PackageNum = IntToBinaryString((int)atof(Task_PackageNum.c_str()), 5);
+		Task_PackageNum = IntToBinaryString(atof(Task_PackageNum.c_str()), 5);
 		Package_Index = operands.at(1); //0开始计数
-		Package_Index = IntToBinaryString((int)atof(Package_Index.c_str()), 5);
+		Package_Index = IntToBinaryString(atof(Package_Index.c_str()), 5);
 		Iteration_PEA = operands.at(2); 
-		Iteration_PEA = IntToBinaryString((int)atof(Iteration_PEA.c_str()), 7);
+		Iteration_PEA = IntToBinaryString(atof(Iteration_PEA.c_str()), 7);
 		Iteration_PE = operands.at(3);
-		Iteration_PE = IntToBinaryString((int)atof(Iteration_PE.c_str()), 7);
+		iteration_pe_temp = atof(Iteration_PE.c_str());
+		if (iteration_pe_temp >= 128) {
+			iteration_pe_temp = iteration_pe_temp - 128;
+			Iteration_PE_ex = "1";
+		}
+		else Iteration_PE_ex = "0";
+		Iteration_PE = IntToBinaryString(iteration_pe_temp, 7);
 		Initial_Idle = operands.at(4);
-		Initial_Idle = IntToBinaryString((int)atof(Initial_Idle.c_str()), 6);
+		Initial_Idle = IntToBinaryString(atof(Initial_Idle.c_str()), 8);
+		Initial_Idle_ex = Initial_Idle.substr(0, 2);
+		Initial_Idle = Initial_Idle.substr(2,6);
 		Iteration_Line = operands.at(5);
-		Iteration_Line = IntToBinaryString((int)atof(Iteration_Line.c_str()), 2);
+		Iteration_Line = IntToBinaryString(atof(Iteration_Line.c_str()), 2);
 		Count = operands.at(6);
-		int intCount = (int)atof(Count.c_str());
+		int intCount = atof(Count.c_str());
 		Count = IntToBinaryString(intCount, 6);
-		if (Assembler::ifHEX) {
-			transformed = ConfigExtend + Func + Task_PackageNum + "000" + Package_Index + "000" +
-				Index_PE + Iteration_PEA + Iteration_PE +
-				Initial_Idle + Iteration_Line + "000000000" + Count;
-			string result("");
-			for (int j = 0; j != 16; j++)
-			{
-				int dec = (int)B2D(transformed.substr(4 * j, 4));
-				string hex = D2H(dec);
-				result = result + hex;
-			}
-			transformed = result;
+#ifdef  HEX_OUTPUT
+		transformed = ConfigExtend +  Func +  Task_PackageNum +  "000" + Package_Index + "000" +
+			Index_PE +  Iteration_PEA + Iteration_PE + 
+			Initial_Idle +  Iteration_Line + Initial_Idle_ex + Iteration_PE_ex + "000000" + Count;
+		string result("");
+		for (int j = 0; j != 16; j++)
+		{
+			int dec = B2D(transformed.substr(4 * j, 4));
+			string hex = D2H(dec);
+			result = result + hex;
 		}
-		else {
-			transformed = ConfigExtend + "_" + Func + "_" + Task_PackageNum + "_" + "000" + "_" + Package_Index + "_" + "000" +
-				"_" + Index_PE + "_" + Iteration_PEA + "_" + Iteration_PE + "_" +
-				Initial_Idle + "_" + Iteration_Line + "_" + "00_0_000000" + "_" + Count;
-		}
+		transformed = result;
+#else
+		transformed = ConfigExtend + "_" + Func + "_" + Task_PackageNum + "_" + "000" + "_" + Package_Index + "_" + "000" +
+			"_" + Index_PE + "_" + Iteration_PEA + "_" + Iteration_PE + "_" +
+			Initial_Idle + "_" + Iteration_Line + "_" +  Initial_Idle_ex + "_" + Iteration_PE_ex +"_000000" + "_" + Count;
+#endif //  HEX_OUTPUT
+
+		
 	}
 	return transformed;
 }
+
+
